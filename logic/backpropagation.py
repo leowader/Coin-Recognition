f1ae5666fda5925d33eef3f41af925b8887f76f0
leowader <waderhe@gmail.com>
2024-06-02 17:40:38 -0500

    guradando y cargando modelo

--- logic/backpropagation.py
+++ logic/backpropagation.py
@@ -4,41 +4,46 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-datos = pd.read_excel("logic/data/data.xlsx")
+datos = pd.read_excel("logic/data/500.xlsx")
+x = datos.filter(like='Hz') 
+y = datos.filter(like='Moneda')
 
-x = datos.filter(like='X') 
-y = datos.filter(like='Y')
+datosx = np.array(x, dtype=float)
+datosy = np.array(y, dtype=float)
 
-datosx = np.array(x, dtype=int)
-datosy = np.array(y, dtype=int)
+# normalizando
+maximo = max(datosx)
+print("maximo",maximo)
+# Divide cada número por el máximo
+resultados = [numero / maximo for numero in datosx]
+xNormalizado=np.array(resultados)
+print("resultados nomalizados",xNormalizado)
 
 # Configuración de la red
 model = tf.keras.Sequential([
-    tf.keras.Input(shape=(3,)),# numero de entradas
-    tf.keras.layers.Dense(40,  activation='sigmoid'),  # Capa oculta con 40 neuronas y función de activación sigmoide
-    tf.keras.layers.Dense(30, activation='sigmoid'),  # Capa oculta adicional con 30 neuronas y función de activación sigmoide
-    tf.keras.layers.Dense(3, activation=tf.nn.sigmoid)  # Capa de salida con 3 neurona y función de activación sigmoide
+    tf.keras.Input(shape=(1,)),# numero de entradas
+    tf.keras.layers.Dense(80,  activation='relu'), 
+    tf.keras.layers.Dense(90, activation='relu'),  
+    tf.keras.layers.Dense(1, activation='linear')  
 ])
-# tf.keras.layers.Dense(40, input_dim=3, activation='sigmoid'), #primera capa con el numero de entradas
 
-model.compile(optimizer=tf.keras.optimizers.Adam(0.1), loss='mean_squared_error')
+model.compile(optimizer=tf.keras.optimizers.Adam(0.01), loss='mean_squared_error')
 
 # Entrenar el modelo
-model.fit(datosx, datosy, epochs=500, verbose=0)
+model.fit(xNormalizado, datosy, epochs=1000, verbose=0)
 
 # Datos de prueba
-X2 = np.array([[0, 0, 0],
-               [1, 0, 1],
-               [1, 1, 0],
-               [1, 1, 1]], dtype=int)
+X2 = np.array([[6004]], dtype=float)
+x2Normalizado=[num / maximo for num in X2]
 
-# Hacer predicciones
-predictions = model.predict(X2)
-
+predictions = model.predict(x2Normalizado)
 rounded_predictions = np.round(predictions).astype(int)
 
 print("Salida de la red neuronal")
-print(rounded_predictions)
-
+print(predictions)
 print("Salida deseada:")
-print(datosy)
+print(datosy[4])
+# model.summary()
+# model.save("logic/data/prueba.keras")#guardar modelo una vez entrenado 
+# mimodelo=tf.keras.models.load_model("logic/data/prueba.keras")# importar modelo guardado
+# mimodelo.summary()
